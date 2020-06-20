ba5a1bccb2720acdb66f011580604ba33566425b
ElKholy <aelkholy248@gmail.com>
2020-06-20 22:59:40 +0200

    feat: Add custom heuristic

--- my_custom_player.py
+++ my_custom_player.py
@@ -1,7 +1,11 @@
 
 from sample_players import DataPlayer
+import math
 import random
 
+_WIDTH = 11
+_HEIGHT = 9
+_SIZE = (_WIDTH + 2) * _HEIGHT - 2
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -43,11 +47,15 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
+        #if self.context is None:
+        #    self.context = dict()
         depth = 1
         while True:
             action = self.alpha_beta_search(state, depth)
-            self.queue.put(action)    
-            depth += 1   
+            self.queue.put(action)
+            # used to calculate the maximum depth   
+            #self.context['depth'] = depth 
+            depth += 1
 
     def alpha_beta_search(self, state, depth):
         """ Return the move along a branch of the game tree that
@@ -83,7 +91,7 @@ class CustomPlayer(DataPlayer):
             return state.utility(self.player_id)
 
         if depth <= 0:
-            return self.my_moves_heuristic(state)
+            return self.heuristic(state)
 
         v = float("inf")
         for a in state.actions():
@@ -101,7 +109,7 @@ class CustomPlayer(DataPlayer):
             return state.utility(self.player_id)
 
         if depth <= 0:
-            return self.my_moves_heuristic(state)
+            return self.heuristic(state)
 
         v = float("-inf")
         for a in state.actions():
@@ -110,5 +118,44 @@ class CustomPlayer(DataPlayer):
             alpha = max(alpha, v)
         return v
 
-    def my_moves_heuristic(self, state):
-        return len(state.liberties(state.locs[self.player_id])) - len(state.liberties(state.locs[1-self.player_id]))
+    def heuristic(self, state):
+        """
+        Combines all the heuristics together
+        """
+        return self.baseline_heuristic(state) + 1.5 * self.distance_heuristics(state)
+    
+    def baseline_heuristic(self, state):
+        """
+        baseline heuristic mentioned in the lecture
+        """
+        return (len(state.liberties(state.locs[self.player_id])) - len(state.liberties(state.locs[1-self.player_id])) )
+
+    def distance_heuristics(self, state):
+        """
+        Combines the center and boundaries heuristics
+        """
+        col1 = (state.locs[self.player_id]) % (_WIDTH + 2)
+        row1 = (state.locs[self.player_id]) // (_WIDTH + 2)
+        if state.locs[1- self.player_id]:
+            col2 = (state.locs[1- self.player_id]) % (_WIDTH + 2)
+            row2 = (state.locs[1- self.player_id]) // (_WIDTH + 2)
+        else:
+            col2 = _WIDTH//2
+            row2 = _HEIGHT//2
+        return self.center_heuristic(row1, col1, row2, col2) + 1.5 * self.boundaries_heuristic(row1, col1, row2, col2) 
+    
+    def center_heuristic(self, row1, col1, row2, col2):
+        """
+         Center heuristic measures how far the player’s location is from the center
+        """
+        my_dist = abs(col1  - _WIDTH//2) + abs(row1 - _HEIGHT//2)
+        his_dist = abs(col2  - _WIDTH//2) + abs(row2 - _HEIGHT//2)
+        return (his_dist - my_dist)
+
+    def boundaries_heuristic(self, row1, col1, row2, col2):
+        """
+        Boundaries heuristic measures how far the player’s location is from the 4 walls and takes the minimum one
+        """
+        my_dist = min(_WIDTH - col1, col1, row1, _HEIGHT - row1)
+        his_dist = min(_WIDTH - col2, col2, row2, _HEIGHT - row2)
+        return (my_dist - his_dist)
